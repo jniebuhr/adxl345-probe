152d8d8f1a7aa1740c666bcd73de848e103ba869
Jochen Ullrich <kontakt@ju-hh.de>
2023-05-14 20:26:43 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,7 @@ function run() {
     printf "Installing ADXL345 Probe, please check the docs after installing on how to configure the extension"
     if [ -d "${TARGET_DIR}" ]; then
         printf "Linking extension... "
-        if ln -sf "${SRC_DIR}/adxl345_probe.py" "${TARGET_DIR}/timelapse.py" &> /dev/null; then
+        if ln -sf "${SRC_DIR}/adxl345_probe.py" "${TARGET_DIR}/adxl345_probe.py" &> /dev/null; then
             printf "Installed component"
         else
             printf "Failed installing component"
