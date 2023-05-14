309538bf5440d10e15835a4642ff48a818e183e2
Jochen Ullrich <kontakt@ju-hh.de>
2023-05-14 20:18:25 +0200

    Create install.sh
    Add installation script

---
+++ scripts/install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -Ee
+
+SRC_DIR="$( cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"/ && pwd )"
+DEPENDS_ON=( klipper )
+TARGET_DIR="${HOME}/klipper/klippy/extras"
+PKGLIST=""
+
+function run() {
+    printf "Installing ADXL345 Probe, please check the docs after installing on how to configure the extension"
+    if [ -d "${TARGET_DIR}" ]; then
+        printf "Linking extension... "
+        if ln -sf "${SRC_DIR}/adxl345_probe.py" "${TARGET_DIR}/timelapse.py" &> /dev/null; then
+            printf "Installed component"
+        else
+            printf "Failed installing component"
+        fi
+    fi
+    sudo systemctl restart "klipper.service"
+}
+
+
+run
+exit 0
