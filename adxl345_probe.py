eafd9d4f016b4619dff26a3875fd757a82d1e7c9
Jochen Ullrich <kontakt@ju-hh.de>
2023-07-09 23:51:55 +0100

    feat: Add activate_gcode and deactivate_gcode, set endstop_position

--- adxl345_probe.py
+++ adxl345_probe.py
@@ -6,19 +6,19 @@ REG_INT_MAP = 0x2F
 REG_TAP_AXES = 0x2A
 REG_INT_ENABLE = 0x2E
 REG_INT_SOURCE = 0x30
-REG_OFSZ = 0x20
 
 DUR_SCALE = 0.000625  # 0.625 msec / LSB
 TAP_SCALE = 0.0625 * adxl345.FREEFALL_ACCEL  # 62.5mg/LSB * Earth gravity in mm/s**2
-OFS_SCALE = 0.0156 * adxl345.FREEFALL_ACCEL  # 15.6mg/LSB * Earth gravity in mm/s**2
 
-PROBE_CALIBRATION_TIME = 1.
-ADXL345_REST_TIME = .01
+ADXL345_REST_TIME = .1
 
 
 class ADXL345Probe:
     def __init__(self, config):
         self.printer = config.get_printer()
+        gcode_macro = self.printer.load_object(config, 'gcode_macro')
+        self.activate_gcode = gcode_macro.load_template(config, 'activate_gcode', '')
+        self.deactivate_gcode = gcode_macro.load_template(config, 'deactivate_gcode', '')
         int_pin = config.get('int_pin').strip()
         self.inverted = False
         if int_pin.startswith('!'):
@@ -48,8 +48,8 @@ class ADXL345Probe:
         self.home_wait = self.mcu_endstop.home_wait
         self.query_endstop = self.mcu_endstop.query_endstop
         # Register commands and callbacks
-        gcode = self.printer.lookup_object('gcode')
-        gcode.register_mux_command("SET_ACCEL_PROBE", "CHIP", None, self.cmd_SET_ACCEL_PROBE, desc=self.cmd_SET_ACCEL_PROBE_help)
+        self.gcode = self.printer.lookup_object('gcode')
+        self.gcode.register_mux_command("SET_ACCEL_PROBE", "CHIP", None, self.cmd_SET_ACCEL_PROBE, desc=self.cmd_SET_ACCEL_PROBE_help)
         self.printer.register_event_handler('klippy:connect', self.init_adxl)
         self.printer.register_event_handler('klippy:mcu_identify', self.handle_mcu_identify)
         self.printer.add_object('probe', probe.PrinterProbe(config, self))
@@ -77,6 +77,9 @@ class ADXL345Probe:
     def multi_probe_end(self):
         pass
 
+    def get_position_endstop(self):
+        return self.position_endstop
+
     def _try_clear_tap(self):
         chip = self.adxl345
         tries = 8
@@ -88,18 +91,20 @@ class ADXL345Probe:
         return False
 
     def probe_prepare(self, hmove):
+        self.activate_gcode.run_gcode_from_command()
         chip = self.adxl345
         toolhead = self.printer.lookup_object('toolhead')
         toolhead.flush_step_generation()
+        toolhead.dwell(ADXL345_REST_TIME)
         print_time = toolhead.get_last_move_time()
-        clock = self.adxl345.mcu.print_time_to_clock(print_time + ADXL345_REST_TIME)
+        clock = self.adxl345.mcu.print_time_to_clock(print_time)
         chip.set_reg(REG_INT_ENABLE, 0x00, minclock=clock)
         chip.read_reg(REG_INT_SOURCE)
         chip.set_reg(REG_INT_ENABLE, 0x40, minclock=clock)
         if not chip.is_measuring():
             chip.set_reg(adxl345.REG_POWER_CTL, 0x08, minclock=clock)
         if not self._try_clear_tap():
-            raise self.printer.command_error("ADXL345 tap triggered after move, it may be set too sensitive.")
+            raise self.printer.command_error("ADXL345 tap triggered before move, it may be set too sensitive.")
 
     def probe_finish(self, hmove):
         chip = self.adxl345
@@ -110,6 +115,7 @@ class ADXL345Probe:
         chip.set_reg(REG_INT_ENABLE, 0x00, minclock=clock)
         if not chip.is_measuring():
             chip.set_reg(adxl345.REG_POWER_CTL, 0x00)
+        self.deactivate_gcode.run_gcode_from_command()
         if not self._try_clear_tap():
             raise self.printer.command_error("ADXL345 tap triggered after move, it may be set too sensitive.")
 
